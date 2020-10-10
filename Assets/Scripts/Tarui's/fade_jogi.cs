9fd8c87c71b38c4ba4d75bae6e209e014a65ae1c
TaruK981221 <tarui2412git@gmail.com>
2020-10-10 16:03:11 +0900

    フェード用のスクリプト、プレハブを作成 プレハブ制作用のシーンを追加

---
+++ Assets/Scripts/Tarui's/fade_jogi.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fade_jogi : MonoBehaviour
+{
+    [SerializeField]
+    bool FadeIn = false, FadeOut = false;
+
+    [SerializeField,Header("フェードしている間の時間")]
+    float FadeTime = 1.0f;
+
+    float elapsedTime = 0.0f;
+
+    float r = 0.0f;
+
+    Vector3 startIn = Vector3.zero;
+    Vector3 startOut = new Vector3(4700.0f, 0.0f);
+
+    Vector3 endIn = new Vector3(-4700.0f, 0.0f);
+    Vector3 endOut = Vector3.zero;
+
+    bool FadeStart = false;
+
+    Transform child;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        child = this.transform.GetChild(0);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!FadeStart && (FadeIn || FadeOut))
+        {
+            FadeStart = !FadeStart;
+
+            if(FadeIn)
+            {
+                child.position = startIn;
+            }
+            else
+            {
+                child.position = startOut;
+            }
+        }
+
+
+        if (FadeIn)
+        {
+            elapsedTime += Time.deltaTime;
+
+            r = elapsedTime / FadeTime;
+
+            child.localPosition = Vector3.Lerp(startIn, endIn, r);
+
+            
+            if (elapsedTime >= FadeTime)
+            {
+                FadeIn = !FadeIn;
+
+                elapsedTime = 0.0f;
+
+                FadeStart = !FadeStart;
+            }
+        }
+
+        if(FadeOut)
+        {
+            elapsedTime += Time.deltaTime;
+
+            r = elapsedTime / FadeTime;
+
+            child.localPosition = Vector3.Lerp(startOut, endOut, r);
+
+
+            if (elapsedTime >= FadeTime)
+            {
+                FadeOut = !FadeOut;
+
+                elapsedTime = 0.0f;
+
+                FadeStart = !FadeStart;
+            }
+        }
+    }
+}
