caa7a8ff9c9a43da9a0ce2e4d960b59fb94c1917
ktsuji <aoriochiki@gmail.com>
2020-10-26 13:00:54 +0900

    Update::定規の動きをそれっぽくしました

--- Assets/Scripts/Tsuji's/JougiController.cs
+++ Assets/Scripts/Tsuji's/JougiController.cs
@@ -15,6 +15,7 @@ public class JougiController : MonoBehaviour
     [SerializeField] private GameObject jougiObj;
     [SerializeField] private Vector3 force;
     [SerializeField] private Vector3 torque;
+    [SerializeField] private int rotatePow;
 
     private GameObject mConObj;
     private MouseController mConScript;
@@ -30,7 +31,6 @@ public class JougiController : MonoBehaviour
             rb = jougiObj.GetComponent<Rigidbody>();
         }
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -41,7 +41,8 @@ public class JougiController : MonoBehaviour
                 mConScript.SetClickFlg(false);
                 force = mConScript.GetPushCompetence();
                 DefaultMove();
-                rb.AddForceAtPosition(force, torque);
+                rb.AddTorque(torque);
+                rb.AddForce(force);
             }
         }
     }
@@ -52,32 +53,37 @@ public class JougiController : MonoBehaviour
         //重心を求める
         var centar = jougiObj.transform.position;
         //軸を求める(定規の正面と合わせてね)
-        var axis = Vector3.left;
+        var axisForward = jougiObj.transform.forward;
+        var axisRight = jougiObj.transform.right;
         //マウスの移動距離取得
         var release = mConScript.GetMouseRelease();
         var distance = centar - release;
         //二つのベクトルのなす角度を求める
-        var angle = Vector3.Angle(axis, distance);
+        var angle = Vector3.Angle(axisForward, distance);
+        var angle2 = Vector3.Angle(axisRight, distance);
         //90°の値を0とする
-        angle -= 90.0f;
-        Debug.Log(angle);
-        torque = Vector3.zero;
-        //if (angle < 0)
-        //{
-        //    //クリックした地点からみて左側にスライドさせた
-        //    torque.y = angle;
+        angle  -= 90.0f;
+        angle2 -= 90.0f;
+        //回転量を計算
+        angle *= rotatePow;
+        //一定の角度以上になると真っ直ぐ飛ばすようにする
+        if (Mathf.Abs(angle) > 80 * rotatePow)
+        {
+            torque = Vector3.zero;
+        }
+        //マウスを離した位置と中心点の距離ベクトル&定規の右向きベクトルとの角度が
+        //＋かーかで回転方向を決める
+        else if (angle2 > 0)
+        {
+            Debug.Log(angle);
 
-        //}
-        //else if (angle > 0)
-        //{
-        //    //クリックした地点から見て右側にスライドさせた
+            torque = new Vector3(0.0f, angle, 0.0f);
+        }
+        else
+        {
+            Debug.Log(-angle);
 
-        //    torque.y = angle;
-        //}
-        //else
-        //{
-        //    //クリックした地点からみてズレがなくスライドした。
-        //    torque = Vector3.zero;
-        //}
+            torque = new Vector3(0.0f, -angle, 0.0f);
+        }
     }
 }
