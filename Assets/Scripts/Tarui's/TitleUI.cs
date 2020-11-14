59e04716e89f58beeb9f41d1c1c5a0cf8cf6aa94
TaruK981221 <tarui2412git@gmail.com>
2020-11-14 11:00:03 +0900

    タイトルの仮UIを追加 カーソルのアニメーションを追加 画像を編集、追加 画僧をファイル分けした

---
+++ Assets/Scripts/Tarui's/TitleUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleUI : MonoBehaviour
+{
+    Transform cursor;
+    Animator cAnim = null;
+    
+    void Awake()
+    {
+        cursor = GameObject.Find("Cursor").transform;
+        cAnim = cursor.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetKeyDown(KeyCode.DownArrow) ||
+            Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if(cAnim.GetBool("Cursor"))
+            {
+                cAnim.SetBool("Cursor", false);
+            }
+            else
+            {
+                cAnim.SetBool("Cursor", true);
+            }
+        }
+    }
+}
